5324aac6facda94f79678186b7343dbe9e5c9059
Administrador <Administrador@junior-PC>
2014-04-19 10:26:27 -0300

    Teste commit

---
+++ SClient/SClient/src/SClientApp.java
@@ -0,0 +1,11 @@
+
+public class SClientApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow word!");
+		System.out.println("Hellow GitHub!");
+
+	}
+
+}
